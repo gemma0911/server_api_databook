f2b9707769d256b9e638731a01584e6911cdb297
thang1304 <thangtq.21it@vku.udn.vn>
2023-02-12 10:43:07 +0700

    first

--- src/index.js
+++ src/index.js
@@ -1,6 +1,6 @@
 import express from 'express'
 import loginR from '../app/routes/RegisterRouter'
-import route1 from '../app/controller/login'
+import con from '../app/config/database'
 const app = express()
 const port = 3000
 app.use(express.json())
@@ -9,10 +9,10 @@ loginR(app)
 
 
 app.get('/', (req, res) => {
-  res.send("hello")
+  con.query('SELECT * FROM user', function (err, rows) {
+    res.json(rows)
+  })
 })
-
-app.use('/v2',route1)
 
 app.listen(port, () => {
   console.log(`Example app listening on port ${port}`)
